8f292b1ee7d6062ddfa31f8010b5e4a1a2e0ee8e
Oksana Gluschenko <oksana_a_g@ukr.net>
2023-04-05 21:56:38 +0300

    les3 Project setting. Debugging.

--- src/app.ts
+++ src/app.ts
@@ -13,92 +13,92 @@
 
 // les2
 
-let age: number;
-age = 50;
-let name: string;
-name = "Max";
-let toggle: boolean;
-toggle = true;
-let empty: null;
-empty = null;
-let notInitialize: undefined;
-notInitialize = undefined;
-let callback = (a: number) => number;
-callback = (a) => { return 100 + a };
+// let age: number;
+// age = 50;
+// let name: string;
+// name = "Max";
+// let toggle: boolean;
+// toggle = true;
+// let empty: null;
+// empty = null;
+// let notInitialize: undefined;
+// notInitialize = undefined;
+// let callback = (a: number) => number;
+// callback = (a) => { return 100 + a };
 
 
-let anything: any;
-anything = -20;
-anything = 'Text';
-anything = {};
+// let anything: any;
+// anything = -20;
+// anything = 'Text';
+// anything = {};
 
 
-let some:unknown;
-some = 'Text';
-let str: string;
-if (typeof some === "string") {
-str = some;
-};
+// let some:unknown;
+// some = 'Text';
+// let str: string;
+// if (typeof some === "string") {
+// str = some;
+// };
 
 
-let person: [string, number];
-person = ['Max', 21];
-
-
-
-enum Load {LOADING, READY};
-const page = {
-  load: Load.READY
-}
-if (page.load === Load.LOADING) {
-  console.log('Страница загружается');
-}
-if (page.load === Load.READY) {
-  console.log('Страница загружена');
-}
+// let person: [string, number];
+// person = ['Max', 21];
 
 
 
-function showMessage(message: string): void {
-  console.log(message);
-}
-function calc(num1: number, num2: number): number {
-  return num1 + num2;
-}
-function customError(): never{
-  throw new Error('Error');
-}
-console.log(9, 9);
+// enum Load {LOADING, READY};
+// const page = {
+//   load: Load.READY
+// }
+// if (page.load === Load.LOADING) {
+//   console.log('Страница загружается');
+// }
+// if (page.load === Load.READY) {
+//   console.log('Страница загружена');
+// }
+
+
+
+// function showMessage(message: string): void {
+//   console.log(message);
+// }
+// function calc(num1: number, num2: number): number {
+//   return num1 + num2;
+// }
+// function customError(): never{
+//   throw new Error('Error');
+// }
+// console.log(9, 9);
 
 
 
 
 
-type PageType =  {
-  title: string,
-  likes: number,
-  accounts: string[],
-  status: 'open' | 'close';
-  details?: {
-    createAt: Date;
-    updateAt: Date;
-  }
-}
+// type PageType =  {
+//   title: string,
+//   likes: number,
+//   accounts: string[],
+//   status: 'open' | 'close';
+//   details?: {
+//     createAt: Date;
+//     updateAt: Date;
+//   }
+// }
 
-const page1 = {
-  title: 'The awesome page',
-  likes: 100,
-  accounts: ['Max', 'Anton', 'Nikita'],
-  status: 'open',
-  details: {
-    createAt: '2021-01-01',
-    updateAt: '2021-05-01',
-  }
-}
+// const page1 = {
+//   title: 'The awesome page',
+//   likes: 100,
+//   accounts: ['Max', 'Anton', 'Nikita'],
+//   status: 'open',
+//   details: {
+//     createAt: '2021-01-01',
+//     updateAt: '2021-05-01',
+//   }
+// }
 
-const page2 = {
-  title: 'Python or Js',
-  likes: 5,
-  accounts: ['Alex'],
-  status: 'close',
-}
+// const page2 = {
+//   title: 'Python or Js',
+//   likes: 5,
+//   accounts: ['Alex'],
+//   status: 'close',
+// }
--- src/buttonAction.ts
+++ src/buttonAction.ts
@@ -1,7 +1,7 @@
-// const button = document.querySelector('button')!;
+const button = document.querySelector('button')!;
 
-// if (button) {
-//   button.addEventListener('click', () => {
-//     console.log('Click');
-//   });
-// }
+if (button) {
+  button.addEventListener('click', () => {
+    console.log('Click');
+  });
+}
